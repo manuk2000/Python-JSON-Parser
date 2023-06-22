60492a60938bb476d447a42cd3a6a1d7fbcbf3ea
manuk2000 <asatryanmanuk2000@gmail.com>
2023-06-22 10:48:12 +0400

    random generate json file version two

--- generate_JSON.py
+++ generate_JSON.py
@@ -1,26 +1,29 @@
 import random
 
 
-def random_json_str():
+def random_json_str(count_dict=2):
     str1 = "{\n   " + \
-        generate_json_str().replace('\n', '\n   ')[0:-3] + '}'
+        generate_json_str(count_dict).replace('\n', '\n   ')[0:-3] + '}'
     return str1
 
-def generate_json_str(count_inerr_dict=1,next_is_dict = True, indent=0):
+def generate_json_str(count_dict=0,next_is_dict = True, indent=0):
+    if count_dict < 1 :
+        print("Error mistake count_dict: " + str(count_dict))
+        return ""
     num_elements = random.randint(1, 3)
     res = ""
     indentation = "   " * indent
     for i in range(num_elements):
         key = f"key_{i + 1}"
         res += f"{indentation}{repr(key)}: "
-        if count_inerr_dict < 3 and next_is_dict:
+        if count_dict > 1 and next_is_dict:
             res += "{\n"
-            res += generate_json_str(count_inerr_dict + 1,True, indent + 1)
+            res += generate_json_str(count_dict - 1,True, indent + 1)
             res += f"{indentation}}},\n"
         else:
             if i == num_elements - 1:
-                count_inerr_dict -= 1
-            next_is_dict = True if count_inerr_dict == 1 else False
+                count_dict += 1
+            next_is_dict = True if count_dict == 3 else False
             value = f"value_{i + 1}"
             res += f"{repr(value)},\n"
     return res
@@ -31,6 +34,6 @@ def write_file(file_path, data):
 
 
 test_file_json = "test_json.txt"
-test_json = random_json_str()
+test_json = random_json_str(6)
 write_file( test_file_json, test_json)
-print(random_json_str())
+print(test_json)
